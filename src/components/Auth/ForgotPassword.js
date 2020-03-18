58c68d4e46481ae0188a9358e652912685f8a814
Alessandro <vyseclown@gmail.com>
2020-03-18 12:03:54 -0300

    Password Reset and CRUD of links completed

--- src/components/Auth/ForgotPassword.js
+++ src/components/Auth/ForgotPassword.js
@@ -4,12 +4,18 @@ import FirebaseContext from './../../firebase/context';
 function ForgotPassword() {
   const { firebase } = useContext(FirebaseContext);
   const [resetPasswordEmail, setResetPasswordEmail] = useState('');
+  const [isPasswordReset, setIsPasswordReset] = useState(false);
+  const [passwordResetError, setPasswordResetError] = useState(null);
 
   async function handleResetPassword() {
     try {
       await firebase.resetPassword(resetPasswordEmail);
+      setIsPasswordReset(true);
+      setPasswordResetError(null);
     } catch (err) {
       console.error('Error sending email', err);
+      setPasswordResetError(err.message);
+      setIsPasswordReset(false);
     }
   }
 
@@ -26,6 +32,10 @@ function ForgotPassword() {
           Reset Password
         </button>
       </div>
+      {isPasswordReset && <p>Check email to reset password</p>}
+      {passwordResetError && (
+        <p className="error-text">{passwordResetError}</p>
+      )}
     </div>
   );
 }
